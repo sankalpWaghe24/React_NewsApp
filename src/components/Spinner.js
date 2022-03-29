217e16d79c58294054bd470f27af49aac0b34a3c
Sankalp Waghe <sankalpwaghe123@gmail.com>
2022-03-29 22:34:16 +0530

    first commit

---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import loading from './loading.gif'
+
+const Spinner = () => {
+    return (
+        <div className="text-center" >
+            <img src={loading} alt="loading" />
+        </div>
+    )
+}
+// cfe12fc33146492d873d42d18fe34009
+export default Spinner
